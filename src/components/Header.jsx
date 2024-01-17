f3cd1965eea96a4a672b9739d767027d398da94a
Qohelet <benjy.shuter@gmail.com>
2024-01-16 21:39:37 -0500

    Adds the header route.

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    <header>
+        <div className="title-box">
+            <h1>Caphtori</h1>
+            <h2>Fullstack Developer</h2>
+        </div>
+    </header>
+};
+
+export default Header;
