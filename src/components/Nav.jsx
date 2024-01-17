dab051aeb63e8bb96ce08798cfef768e0f8a4d75
Qohelet <benjy.shuter@gmail.com>
2024-01-16 21:15:24 -0500

    Begins the incomplete navbar.

---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+function Nav({ thisPage }){
+    const tabs = ['portfolio', 'resume', 'contact']
+    return(
+        <nav className='header-nav'>
+            <ul>
+                <li>
+                    <Link to='/'>About</Link>
+                </li>
+            </ul>
+            {tabs.map((tab)=>{
+                <li>
+                    <Link to={`/${tab}`}>{tab}</Link>
+                </li> 
+            })}
+        </nav>
+    )
+}
