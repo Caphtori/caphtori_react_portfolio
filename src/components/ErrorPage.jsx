59bd194053efa8cfbb67b12c10563549aa62fe48
Qohelet <benjy.shuter@gmail.com>
2024-01-16 19:32:37 -0500

    Adds an error page.

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import { useRouteError } from "react-router-dom"
+
+export default function ErrorPage(){
+    const err = useRouteError();
+    console.error(err);
+
+    return(
+        <section className="error-page">
+            <h2>Uh Oh!</h2>
+            <p>Looks like an error occured.  It's probably your fault, but I won't tell anyone...</p>
+            <p>{err.statusText || err.message}</p>
+        </section>
+    );
+};
